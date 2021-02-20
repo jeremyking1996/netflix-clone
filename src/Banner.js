91eaaa8f1a3ecefed2a17933a0663566c18d590d
Jeremy King <jeremy.david.king.1996@gmail.com>
2021-02-20 00:03:03 -0500

    done with banner, struggling loading background-image

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 
 function Banner() {
@@ -19,10 +20,35 @@ function Banner() {
         fetchData();
     }, []);
 
+    function truncate(str, n){
+        return str?.length > 50 ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
-        <div>
-        
+        <header className="banner"
+        style={{
+            backgroundSize: "cover",
+            bacgkroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path || movie?.poster_path}"
+            )`,
+            backgroundPosition: "center center",
+        }}
+        >
+        <div className="banner_contents">
+        <h1 className="banner_title">
+            { movie?.title || movie?.name || movie?.original_name }
+        </h1>
+        <div className="banner_buttons">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">My List</button>
         </div>
+        <h1 className="banner_description">
+            {movie?.overview}
+        </h1>
+
+        </div>
+        <div classRoom="fade_bottom" />
+        </header>
     );
 };
 
